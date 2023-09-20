aab8ada49ebc862f95408e0a1b6115a062338abb
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-09-21 03:49:04 +0800

    feat: add flip card component

---
+++ components/app-flip-card.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flip-card">
+    <div class="flip-card-inner">
+      <div class="flip-card-front">
+        <slot name="front"></slot>
+      </div>
+      <div class="flip-card-back">
+        <slot name="back"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.flip-card {
+  perspective: 1000px;
+}
+
+.flip-card-inner {
+  @apply relative w-full h-full transition-transform duration-700;
+  transform-style: preserve-3d;
+}
+
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+.flip-card-front,
+.flip-card-back {
+  @apply absolute w-full h-full;
+  backface-visibility: hidden;
+}
+
+.flip-card-back {
+  transform: rotateY(180deg);
+}
+</style>
