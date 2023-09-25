4fc89f1790fd7e8dee730d46e2cd859725a3f7fa
Timothy Adams <timothyadamsjenuil@gmail.com>
2023-09-25 12:23:22 +0800

    feat: add carousel for exp and edu sections

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,21 @@
   <div class="flex flex-col justify-between min-h-screen">
     <TheHeader />
     <main class="flex-1">
-      <HeroGreets />
-      <HeroExp />
-      <HeroProjects />
+      <HeroGreets id="about" />
+      <div class="carousel w-full">
+        <div id="experience" class="carousel-item w-full">
+          <HeroExp />
+        </div>
+        <div id="education" class="carousel-item w-full">
+          <HeroEdu />
+        </div>
+      </div>
+      <div class="flex justify-center gap-1">
+        <a href="#experience" role="button" class="btn btn-xs btn-circle">1</a>
+        <a href="#education" role="button" class="btn btn-xs btn-circle">2</a>
+      </div>
+      <HeroProjects id="projects" />
     </main>
     <TheFooter />
   </div>
 </template>
-
-<script>
-import HeroGreets from "~/components/hero-greets.vue";
-
-export default { components: { HeroGreets } };
-</script>
