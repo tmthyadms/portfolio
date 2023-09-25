cf1365d3d2dbd30fd77ebf5431c3cc6edf681dc8
Timothy Adams <timothyadamsjenuil@gmail.com>
2023-09-25 12:19:38 +0800

    refactor: update project adding

--- components/hero-projects.vue
+++ components/hero-projects.vue
@@ -1,54 +1,26 @@
 <template>
-  <AppHero title="Projects" class="pb-14 md:pb-0">
-    <div class="grid md:grid-cols-2 gap-6">
-      <AppFlipCard>
+  <AppHero title="Projects">
+    <div
+      class="grid grid-cols-[minmax(50vw,_387px)] grid-rows-[587.9px] auto-rows-[587.9px] lg:grid-cols-[repeat(2,_minmax(387px,_1fr))] lg:grid-flow-cols gap-6"
+    >
+      <AppFlipCard v-for="(project, index) in projects" :key="index">
         <template #front>
           <AppCard
-            title="Sentinel"
-            desc="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-            :newProject="true"
-            :badges="['Online exam', 'AI proctoring']"
+            :title="project.title"
+            :desc="project.desc"
+            :imgSrc="project.imgSrc"
+            :badge="project.badge"
+            :badges="project.badges"
           />
         </template>
         <template #back>
           <p>
             Learn more:
             <span
-              class="tooltip tooltip-bottom"
-              data-tip="https://github.com/tmthyadms/sentinel"
+              ><a :href="project.url" class="link link-accent">{{
+                project.title
+              }}</a></span
             >
-              <a
-                href="https://github.com/tmthyadms/sentinel"
-                target="_blank"
-                class="link link-info"
-                >Sentinel</a
-              >
-            </span>
-          </p>
-        </template>
-      </AppFlipCard>
-      <AppFlipCard>
-        <template #front>
-          <AppCard
-            title="LockNRoll"
-            desc="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-            :badges="['Smart home', 'IoT', 'Mobile app']"
-          />
-        </template>
-        <template #back>
-          <p>
-            Learn more:
-            <span
-              class="tooltip tooltip-bottom"
-              data-tip="https://github.com/tmthyadms/locknroll"
-            >
-              <a
-                href="https://github.com/tmthyadms/locknroll"
-                target="_blank"
-                class="link link-info"
-                >LockNRoll</a
-              >
-            </span>
           </p>
         </template>
       </AppFlipCard>
@@ -57,19 +29,37 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          title: "Sentinel",
+          desc: "An AI-powered virtual assessment proctoring.",
+          imgSrc: "https://placehold.co/512",
+          badge: "NEW",
+          badges: ["PHP 8", "Bootstrap 5", "JavaScript", "MariaDB"],
+          url: "https://github.com/tmthyadms/sentinel",
+        },
+        {
+          title: "LockNRoll",
+          desc: "A mobile app for controlling custom-built smart door lock remotely.",
+          imgSrc: "https://placehold.co/512",
+          badges: ["Flutter", "Firebase", "Arduino"],
+          url: "https://github.com/tmthyadms/locknroll",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
-:deep(.flip-card) {
-  @apply w-[372px] h-[548px];
-}
-
 :deep(.flip-card-inner) {
-  @apply rounded-2xl shadow-xl shadow-[hsl(var(--sf))]/[0.25];
+  @apply shadow-xl;
 }
 
 :deep(.flip-card-back) {
-  @apply grid place-content-center rounded-[inherit];
+  @apply grid place-content-center;
 }
 </style>
