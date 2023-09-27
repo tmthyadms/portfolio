c49c72ffd2c29a092cb3b1209043e401802edf10
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-09-27 12:11:37 +0800

    fix: update height to dynamic

--- components/app-flip-card.vue
+++ components/app-flip-card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flip-card">
+  <div :id="flipIDAttr" class="flip-card" @click="flip">
     <div class="flip-card-inner">
       <div class="flip-card-front">
         <slot name="front"></slot>
@@ -11,28 +11,68 @@
   </div>
 </template>
 
+<script>
+export default {
+  props: {
+    flipID: {
+      type: Number,
+      default: 0,
+    },
+    flipName: {
+      type: String,
+      default: "def",
+    },
+  },
+  data() {
+    return {
+      flipIDAttr: null,
+    };
+  },
+  computed: {
+    newFlipID() {
+      return `flip-card-${this.flipName}-${this.flipID}`;
+    },
+  },
+  mounted() {
+    this.flipIDAttr = this.newFlipID;
+  },
+  methods: {
+    flip() {
+      const flipCard = document.getElementById(this.flipIDAttr);
+      console.log(flipCard);
+      flipCard.classList.toggle("clicked");
+    },
+  },
+};
+</script>
+
 <style scoped>
 .flip-card {
   @apply card;
   perspective: 1000px;
 }
 
-.flip-card-inner {
+.flip-card .flip-card-inner {
   @apply relative w-full h-full rounded-[inherit] transition-transform duration-700;
   transform-style: preserve-3d;
 }
 
-.flip-card:hover .flip-card-inner {
+.flip-card.clicked .flip-card-inner {
   transform: rotateY(180deg);
 }
 
 .flip-card-front,
 .flip-card-back {
-  @apply absolute w-full h-full rounded-[inherit];
+  @apply rounded-[inherit];
   backface-visibility: hidden;
 }
 
+.fip-card-front {
+  @apply relative;
+}
+
 .flip-card-back {
+  @apply absolute top-0 right-0 w-full h-full;
   transform: rotateY(180deg);
 }
 
