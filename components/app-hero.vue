7f818c8406c187204dde2032c27a633c125817d5
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-09-27 12:13:21 +0800

    feat: matching desc color and update title margin

--- components/app-hero.vue
+++ components/app-hero.vue
@@ -2,8 +2,8 @@
   <div class="hero min-h-screen bg-base-theme">
     <div class="hero-content flex-col text-center">
       <div class="max-w-lg">
-        <h1 class="text-5xl font-bold pb-6">{{ title }}</h1>
-        <p v-if="desc">
+        <h1 class="text-5xl font-bold mb-4">{{ title }}</h1>
+        <p v-if="desc" class="mb-6 opacity-60">
           {{ desc }}
         </p>
       </div>
