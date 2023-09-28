486122e8ca35b15bc97470c672b2dac00f37edad
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-09-28 19:30:31 +0800

    refactor: extract carousel into component

---
+++ components/app-carousel.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="carousel" :class="{ 'w-full': fullWidth }">
+      <div
+        v-for="(item, index) in items"
+        :key="index"
+        :id="item.id"
+        class="carousel-item"
+        :class="{ 'w-full': fullWidth }"
+      >
+        <component :is="item.component" />
+      </div>
+    </div>
+    <div class="flex justify-center gap-1">
+      <a
+        v-for="(item, index) in items"
+        :key="index"
+        :href="`#${item.id}`"
+        role="button"
+        class="btn btn-xs btn-circle"
+        >{{ index + 1 }}</a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+    fullWidth: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,36 +2,42 @@
   <div class="flex flex-col justify-between min-h-screen">
     <TheHeader />
     <main class="flex-1">
+      <!-- Top section -->
       <HeroGreets id="about" />
-      <div class="carousel w-full">
-        <div id="experience" class="carousel-item w-full">
-          <HeroExp />
-        </div>
-        <div id="education" class="carousel-item w-full">
-          <HeroEdu />
-        </div>
-      </div>
-      <div class="flex justify-center gap-1">
-        <a href="#experience" role="button" class="btn btn-xs btn-circle">1</a>
-        <a href="#education" role="button" class="btn btn-xs btn-circle">2</a>
-      </div>
-      <div class="carousel w-full">
-        <div id="main-projects" class="carousel-item w-full">
-          <HeroMainProjects />
-        </div>
-        <div id="side-projects" class="carousel-item w-full">
-          <HeroSideProjects />
-        </div>
-      </div>
-      <div class="flex justify-center gap-1">
-        <a href="#main-projects" role="button" class="btn btn-xs btn-circle"
-          >1</a
-        >
-        <a href="#side-projects" role="button" class="btn btn-xs btn-circle"
-          >2</a
-        >
-      </div>
+      <!-- Mid section -->
+      <AppCarousel :items="midCarouselItems" :full-width="true" />
+      <!-- Bot section -->
+      <AppCarousel :items="botCarouselItems" :full-width="true" />
     </main>
     <TheFooter />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      midCarouselItems: [
+        {
+          id: "experience",
+          component: "HeroExp",
+        },
+        {
+          id: "education",
+          component: "HeroEdu",
+        },
+      ],
+      botCarouselItems: [
+        {
+          id: "main-projects",
+          component: "HeroMainProjects",
+        },
+        {
+          id: "side-projects",
+          component: "HeroSideProjects",
+        },
+      ],
+    };
+  },
+};
+</script>
