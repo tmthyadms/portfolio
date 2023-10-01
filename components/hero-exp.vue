3fba0a829c7a98b6d0b9b924d52efa7fdc079a7c
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-10-02 00:48:24 +0800

    refactor: extract experience data into object

--- components/hero-exp.vue
+++ components/hero-exp.vue
@@ -1,36 +1,36 @@
 <template>
   <AppHero title="Experience">
-    <h2 class="font-semibold">
-      Frontend Intern @
-      <span
-        class="tooltip tooltip-bottom"
-        data-tip="https://benefitsolutions.com.sg"
-      >
-        <a
-          href="https://benefitsolutions.com.sg"
-          target="_blank"
-          class="link link-accent"
-          >Benefit Solutions Pte Ltd</a
+    <div v-for="(exp, index) in exps" :key="index">
+      <h2 class="font-semibold">
+        {{ exp.role }} @
+        <span class="tooltip tooltip-bottom" :data-tip="exp.companyUrl">
+          <a :href="exp.companyUrl" target="_blank" class="link link-accent">{{
+            exp.company
+          }}</a>
+        </span>
+      </h2>
+      <p class="mb-2">
+        <span class="align-middle opacity-60">{{ exp.duration }}&nbsp;</span>
+        <!-- TODO: calculate duration -->
+        <span class="tooltip tooltip-right align-middle" data-tip="6 months">
+          <SvgInfoCircle class="opacity-60" />
+        </span>
+      </p>
+      <p class="mb-6 text-lg opacity-60">
+        <span v-if="exp?.desc">{{ exp.desc }}</span>
+        <template v-else>
+          <span class="italic">Updating soon</span>
+          <span class="loading loading-dots loading-sm align-bottom"></span>
+        </template>
+      </p>
+      <div class="flex justify-center gap-2">
+        <div
+          v-for="(skill, index) in exp.skills"
+          :key="index"
+          class="badge badge-lg badge-success font-semibold"
         >
-      </span>
-    </h2>
-    <p class="mb-2">
-      <span class="align-middle opacity-60">Aug 2023 - Jan 2024&nbsp;</span>
-      <span class="tooltip tooltip-right align-middle" data-tip="6 months">
-        <SvgInfoCircle class="opacity-60" />
-      </span>
-    </p>
-    <p class="mb-6 text-lg opacity-60">
-      <span class="italic">Updating soon</span>
-      <span class="loading loading-dots loading-sm align-bottom"></span>
-    </p>
-    <div class="flex justify-center gap-2">
-      <div
-        v-for="(skill, index) in skills"
-        :key="index"
-        class="badge badge-lg badge-success font-semibold"
-      >
-        {{ skill }}
+          {{ skill }}
+        </div>
       </div>
     </div>
   </AppHero>
@@ -40,7 +40,16 @@
 export default {
   data() {
     return {
-      skills: ["Nuxt.js", "Vue.js", "Gitlab"],
+      exps: [
+        {
+          role: "Frontend Intern",
+          company: "Benefit Solutions Pte Ltd",
+          companyUrl: "https://www.benefitsolutions.com.sg",
+          duration: "Aug 2023 - Jan 2024",
+          desc: "",
+          skills: ["Nuxt.js", "Vue.js", "Gitlab"],
+        },
+      ],
     };
   },
 };
