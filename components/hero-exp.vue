8a38d244b4ad77f404076b6f250def10f6787a59
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-10-02 17:48:10 +0800

    feat: add exp as stack card and briefcase icon

--- components/hero-exp.vue
+++ components/hero-exp.vue
@@ -1,37 +1,9 @@
 <template>
   <AppHero title="Experience">
-    <div v-for="(exp, index) in exps" :key="index">
-      <h2 class="font-semibold">
-        {{ exp.role }} @
-        <span class="tooltip tooltip-bottom" :data-tip="exp.companyUrl">
-          <a :href="exp.companyUrl" target="_blank" class="link link-accent">{{
-            exp.company
-          }}</a>
-        </span>
-      </h2>
-      <p class="mb-2">
-        <span class="align-middle opacity-60">{{ exp.duration }}&nbsp;</span>
-        <!-- TODO: calculate duration -->
-        <span class="tooltip tooltip-right align-middle" data-tip="6 months">
-          <SvgInfoCircle class="opacity-60" />
-        </span>
-      </p>
-      <p class="mb-6 text-lg opacity-60">
-        <span v-if="exp?.desc">{{ exp.desc }}</span>
-        <template v-else>
-          <span class="italic">Updating soon</span>
-          <span class="loading loading-dots loading-sm align-bottom"></span>
-        </template>
-      </p>
-      <div class="flex justify-center gap-2">
-        <div
-          v-for="(skill, index) in exp.skills"
-          :key="index"
-          class="badge badge-lg badge-success font-semibold"
-        >
-          {{ skill }}
-        </div>
-      </div>
+    <div class="stack">
+      <template v-for="(exp, index) in exps">
+        <CardExp :key="index" :exp="exp" />
+      </template>
     </div>
   </AppHero>
 </template>
