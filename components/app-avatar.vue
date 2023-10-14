717149c296f20fb52e3ad1bed8d6f803e9db8670
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-10-14 10:53:51 +0800

    feat: update responsiveness

--- components/app-avatar.vue
+++ components/app-avatar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="avatar" :class="hasLoaded ? 'block' : 'hidden'">
-      <div class="avatar-ring group w-64 rounded-full">
+      <div class="avatar-ring group w-48 md:w-64 rounded-full">
         <img
           src="profile-pic.jpg"
           alt="Profile picture"
@@ -12,7 +12,7 @@
     </div>
     <div class="avatar placeholder" :class="hasLoaded ? 'hidden' : 'block'">
       <div
-        class="avatar-ring group w-64 text-neutral-content bg-neutral-focus rounded-full"
+        class="avatar-ring group w-48 md:w-64 text-neutral-content bg-neutral-focus rounded-full"
       >
         <span
           class="text-7xl group-hover:scale-150 transition-transform duration-500 ease-out"
