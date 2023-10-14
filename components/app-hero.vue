baf328f8df8f7ab395cea3f49e01bc534d604f65
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-10-14 10:37:22 +0800

    feat: use v-html for desc

--- components/app-hero.vue
+++ components/app-hero.vue
@@ -1,13 +1,30 @@
 <template>
-  <div class="hero min-h-screen bg-base-theme">
-    <div class="hero-content flex-col text-center">
-      <div class="max-w-lg">
-        <h1 class="text-5xl font-bold mb-4">{{ title }}</h1>
-        <p v-if="desc" class="mb-6 opacity-60">
-          {{ desc }}
-        </p>
-      </div>
+  <div class="hero min-h-screen">
+    <div
+      class="hero-content flex-col"
+      :class="{
+        'text-center': center,
+        'flex-col lg:flex-row-reverse text-center lg:text-left': !center,
+      }"
+    >
+      <slot v-if="!center" name="figure"></slot>
       <div>
+        <div class="mx-auto max-w-md">
+          <slot name="title">
+            <h1
+              v-if="title"
+              class="text-3xl md:text-4xl lg:text-5xl font-bold capitalize"
+              :class="desc ? 'mb-4' : 'mb-6'"
+            >
+              {{ title }}
+            </h1>
+          </slot>
+          <p
+            v-if="desc"
+            class="mb-6 text-sm md:text-base lg:text-2xl opacity-60"
+            v-html="desc"
+          ></p>
+        </div>
         <slot></slot>
       </div>
     </div>
@@ -22,6 +39,10 @@ export default {
     },
     desc: {
       type: String,
+    },
+    center: {
+      type: Boolean,
+      default: true,
     },
   },
 };
