674d5f36ae34db9f64de89683d952e6b20f09c7a
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-11-19 17:09:08 +0800

    feat: add scroll scripts and update ui
    change logo to button to use scroll script and remove uppercases to
    maintain with daisyUI button case changes

--- components/the-header.vue
+++ components/the-header.vue
@@ -1,12 +1,15 @@
 <template>
   <header
     id="header"
-    class="navbar sticky top-0 z-30 px-4 justify-between bg-base-100 bg-opacity-90 backdrop-blur transition-shadow duration-300"
+    class="navbar sticky top-0 z-30 px-8 justify-between bg-base-100 bg-opacity-90 backdrop-blur transition-shadow duration-300"
   >
     <div class="navbar-start">
       <!-- Mobile navigation -->
       <div class="dropdown lg:hidden mr-2">
-        <label role="button" tabindex="0" class="btn btn-sm btn-ghost mb-1"
+        <label
+          role="button"
+          tabindex="0"
+          class="btn btn-sm btn-square btn-ghost mb-1"
           ><IconList :width="20"
         /></label>
         <ul
@@ -19,36 +22,42 @@
                 v-for="(subsection, index) in section.type"
                 :key="index"
                 :href="subsection.href"
-                class="text-xs md:text-sm font-semibold uppercase"
+                class="text-xs md:text-sm font-semibold"
                 >{{ subsection.title }}</a
               >
             </template>
             <a
               v-else
               :href="section.href"
-              class="text-xs md:text-sm font-semibold uppercase"
+              class="text-xs md:text-sm font-semibold"
               >{{ section.title }}</a
             >
           </li>
         </ul>
       </div>
-      <a
-        role="button"
-        href="#"
-        class="flex items-center text-lg md:text-xl font-bold normal-case"
-        >Timothy&nbsp;<span class="text-accent">Adams&nbsp;</span>
-        <IconPatchCheck fill="hsl(var(--in))" />
-      </a>
+      <button
+        class="flex items-center gap-1 text-lg md:text-xl font-bold"
+        @click="scrollToTop"
+      >
+        <span>Timothy&nbsp;<span class="text-accent">Adams&nbsp;</span></span>
+        <IconPatchCheck class="fill-info" />
+      </button>
     </div>
     <div class="navbar-center hidden lg:flex">
       <!-- Desktop navigation -->
       <template v-for="(section, index) in sections">
-        <div v-if="section?.type" class="dropdown dropdown-hover group">
-          <label role="button" tabindex="0" class="btn btn-ghost font-semibold"
+        <div v-if="section?.type" class="dropdown dropdown-end group">
+          <label
+            role="button"
+            tabindex="0"
+            class="btn btn-sm btn-ghost font-semibold"
             >{{ section.title }}
             <IconCaretDownFill />
           </label>
-          <ul tabindex="0" class="dropdown-content menu menu-design p-2 w-52">
+          <ul
+            tabindex="0"
+            class="dropdown-content menu menu-design app-border mt-1 p-2 w-52"
+          >
             <li v-for="(subsection, index) in section.type" :key="index">
               <a :href="subsection.href" class="subsection-desktop">{{
                 subsection.title
@@ -61,7 +70,7 @@
           :key="index"
           role="button"
           :href="section.href"
-          class="btn btn-ghost font-semibold"
+          class="btn btn-sm btn-ghost font-semibold"
           >{{ section.title }}</a
         >
       </template>
@@ -69,13 +78,10 @@
     <div class="navbar-end">
       <div
         id="tip-theme"
-        class="tooltip tooltip-info tooltip-left"
+        class="tooltip tooltip-primary tooltip-left"
         :data-tip="theme.light.tip"
       >
-        <label
-          role="button"
-          class="btn btn-sm btn-ghost btn-circle swap swap-rotate"
-        >
+        <label role="button" class="btn btn-ghost btn-circle swap swap-rotate">
           <input type="checkbox" v-model="theme.isDarkMode" />
           <div class="swap-off">
             <IconEmojiSunglasses :width="20" />
@@ -165,6 +171,9 @@ export default {
         else header.classList.remove("shadow", "ease-in");
       };
     },
+    scrollToTop() {
+      window.scrollTo({ top: 0 });
+    },
   },
 };
 </script>
@@ -175,6 +184,6 @@ export default {
 }
 
 .subsection-desktop {
-  @apply hover:bg-base-content/[0.1] font-semibold uppercase;
+  @apply hover:bg-base-content/[0.1] font-semibold;
 }
 </style>
