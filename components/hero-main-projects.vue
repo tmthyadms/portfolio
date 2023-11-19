946dd3d1d018c165e056a75daa3ae4379b9c110c
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-11-19 17:31:05 +0800

    feat: add new main project and update aos responsiveness

--- components/hero-main-projects.vue
+++ components/hero-main-projects.vue
@@ -1,12 +1,16 @@
 <template>
   <AppHero title="Main Projects" desc="Learn more by clicking on a project.">
-    <div class="app-flex">
+    <div class="projects">
       <AppFlipCard
         v-for="(mainProject, index) in mainProjects"
         :key="index"
-        :data-aos="index % 2 === 0 ? 'fade-right' : 'fade-left'"
+        :data-aos="aos(index)"
+        data-aos-offset="150"
         :flip-id="index"
         flip-name="main-projects"
+        :class="{
+          'lg:col-span-2 lg:place-self-center': isLast(index) && isEven(index),
+        }"
       >
         <template #front>
           <AppCard
@@ -17,7 +21,7 @@
             :badge="mainProject.badge"
             :badges="mainProject.badges"
             :theme="false"
-            class="h-full"
+            class="card-project"
           />
         </template>
         <template #back>
@@ -33,6 +37,22 @@ export default {
   data() {
     return {
       mainProjects: [
+        {
+          title: "Jobzone",
+          desc: "A website to evaluate fraudulent job posting.",
+          imgSrc: "https://placehold.co/1919x1080?text=Jobzone",
+          info: "Developed for GDG George Town DevHack 2023",
+          badges: [
+            "Nuxt 2",
+            "Tailwind CSS",
+            "daisyUI",
+            "Flask",
+            "MongoDB",
+            "Python",
+            "XGBoost",
+          ],
+          url: "https://github.com/tmthyadms/jobzone",
+        },
         {
           title: "LockNRoll",
           desc: "A mobile app for controlling custom-built smart door lock remotely.",
@@ -51,7 +71,40 @@ export default {
           remarks: "Check out EyeBuddy on my LinkedIn's project.",
         },
       ],
+      anims: {
+        even: "fade-right",
+        odd: "fade-left",
+        last: "fade-down",
+      },
     };
+  },
+  mounted() {
+    const mediaQuery = window.matchMedia("not all and (min-width: 1024px)");
+    this.aosOnMobile(mediaQuery);
+    mediaQuery.addEventListener("change", this.aosOnMobile);
+  },
+  methods: {
+    isEven(index) {
+      return index % 2 === 0;
+    },
+    isLast(index) {
+      return index === this.mainProjects.length - 1;
+    },
+    aos(index) {
+      const isEven = this.isEven(index);
+      const isLast = this.isLast(index);
+      if (isEven) return isLast ? this.anims.last : this.anims.even;
+      else return this.anims.odd;
+    },
+    aosOnMobile(mediaQuery) {
+      if (mediaQuery.matches) {
+        this.anims.even = "fade-down";
+        this.anims.odd = "fade-down";
+      } else {
+        this.anims.even = "fade-right";
+        this.anims.odd = "fade-left";
+      }
+    },
   },
 };
 </script>
