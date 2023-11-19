9ee914ff5c66515bbe1f9ef1a80ea9e3e8512007
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-11-20 00:40:51 +0800

    feat: add asterisk icon

---
+++ components/icon/asterisk.vue
@@ -0,0 +1,37 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="fallbackHeight"
+    :fill="fill"
+    class="bi bi-asterisk"
+    viewBox="0 0 16 16"
+  >
+    <path
+      d="M8 0a1 1 0 0 1 1 1v5.268l4.562-2.634a1 1 0 1 1 1 1.732L10 8l4.562 2.634a1 1 0 1 1-1 1.732L9 9.732V15a1 1 0 1 1-2 0V9.732l-4.562 2.634a1 1 0 1 1-1-1.732L6 8 1.438 5.366a1 1 0 0 1 1-1.732L7 6.268V1a1 1 0 0 1 1-1z"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 16,
+    },
+    height: {
+      type: Number,
+    },
+    fill: {
+      type: String,
+      default: "currentColor",
+    },
+  },
+  computed: {
+    fallbackHeight() {
+      return this.height || this.width;
+    },
+  },
+};
+</script>
