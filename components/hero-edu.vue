955d79b351ee2fb512904822100b7070d7153d80
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-11-19 17:28:31 +0800

    feat: implement timeline edu and add additional edu details
    additional edu details including start/end dates and completion status

--- components/hero-edu.vue
+++ components/hero-edu.vue
@@ -1,7 +1,8 @@
 <template>
   <AppHero title="Education">
-    <AppCard data-aos="flip-down" class="app-border shadow-inner">
-      <StepsEdu :edus="edus" />
+    <AppCard class="app-border max-w-md lg:max-w-none shadow-inner">
+      <!-- <StepsEdu :edus="edus" /> -->
+      <TimelineEdu :edus="edus" class="text-start" />
     </AppCard>
   </AppHero>
 </template>
@@ -14,20 +15,35 @@ export default {
         {
           program: "Bachelor's in Computer Science (Hons.)",
           school: "Management and Science University",
+          date: {
+            start: { year: 2021 },
+            end: { year: 2024 },
+          },
           tip: "CGPA",
-          result: "3.82",
+          result: "3.82/4.00",
+          completed: false,
         },
         {
           program: "Foundation in Computer Science",
           school: "Universiti Teknologi PETRONAS",
+          date: {
+            start: { year: 2019 },
+            end: { year: 2020 },
+          },
           tip: "CGPA",
-          result: "3.86",
+          result: "3.86/4.00",
+          completed: true,
         },
         {
           program: "Pure Science & Computer Science",
           school: "SMK Tansau",
+          date: {
+            start: { year: 2014 },
+            end: { year: 2018 },
+          },
           tip: "SPM",
           result: "8A 1B",
+          completed: true,
         },
       ],
     };
