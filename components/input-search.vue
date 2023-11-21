d8d494a0c577cf633337b0b9aed2a61209e783c9
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-11-21 16:44:48 +0800

    feat: implement quick search

--- components/input-search.vue
+++ components/input-search.vue
@@ -1,25 +1,17 @@
 <template>
   <div class="relative hidden lg:flex max-w-xs">
-    <IconSearch
-      :width="14"
-      class="absolute top-1/2 start-3 transform -translate-y-1/2"
-    />
+    <IconSearch :width="14" class="input-content start-3" />
     <input
-      type="search"
       id="search"
-      placeholder="Search..."
+      type="search"
+      placeholder="Quick search..."
       class="input input-sm input-bordered w-full !ps-8 !pe-[4.25rem]"
-      @input="searchSections($event)"
+      @input="quickSearch"
     />
-    <ul
-      v-if="results.length > 0"
-      class="menu absolute top-10 w-full bg-base-100 app-border rounded-box shadow-inner"
-    >
-      <li v-for="(result, index) in results" :key="index">
-        <a role="button" :href="result.href">{{ result.title }}</a>
-      </li>
-    </ul>
-    <div class="flex gap-1 absolute top-1/2 end-3" style="translate: 0 -50%">
+    <template v-if="results.length > 0">
+      <SearchMenu :results="results" class="absolute top-10 w-full" />
+    </template>
+    <div class="flex gap-1 input-content end-3">
       <kbd class="kbd kbd-xs">ctrl</kbd>
       <kbd class="kbd kbd-xs">k</kbd>
     </div>
@@ -27,6 +19,9 @@
 </template>
 
 <script>
+// import { distance } from "fastest-levenshtein";
+import distance from "jaro-winkler";
+
 export default {
   data() {
     return {
@@ -58,6 +53,7 @@ export default {
         },
       ],
       results: [],
+      searchMenu: null,
     };
   },
   mounted() {
@@ -66,90 +62,51 @@ export default {
   methods: {
     searchKeybind() {
       window.addEventListener("keydown", (event) => {
+        const search = document.getElementById("search");
         if (event.ctrlKey && event.code === "KeyK") {
           event.preventDefault();
-          document.getElementById("search").focus();
+          search.focus();
+        } else if (event.code === "Escape") {
+          this.results = [];
+          search.blur();
         }
       });
     },
-    levenshteinDistance(a, b) {
-      if (a.length === 0) return b.length;
-      if (b.length === 0) return a.length;
-
-      const matrix = [];
-
-      for (let i = 0; i <= b.length; i++) {
-        matrix[i] = [i];
+    updateResults(section, input) {
+      const isIncluded = this.results.some(
+        (result) => result.title === section.title
+      );
+      if (isIncluded) return;
+      const sectionTitle = section.title;
+      const lowerSectionTitle = sectionTitle.toLowerCase();
+      const lowerInput = input.toLowerCase();
+      if (lowerSectionTitle.includes(lowerInput)) {
+        const result = {
+          ...section,
+          ...{
+            distance: distance(sectionTitle, input),
+          },
+        };
+        this.results.push(result);
       }
-
-      for (let j = 0; j <= a.length; j++) {
-        matrix[0][j] = j;
-      }
-
-      for (let i = 1; i <= b.length; i++) {
-        for (let j = 1; j <= a.length; j++) {
-          const cost = a[j - 1] === b[i - 1] ? 0 : 1;
-          matrix[i][j] = Math.min(
-            matrix[i - 1][j] + 1, // deletion
-            matrix[i][j - 1] + 1, // insertion
-            matrix[i - 1][j - 1] + cost // substitution
-          );
-        }
-      }
-
-      return matrix[b.length][a.length];
     },
-    searchSections(event) {
-      const input = event.target.value.toLowerCase();
-      const similarityThreshold = 2; // Adjust this threshold based on your preference
-      const maxResults = 3; // Adjust the maximum number of displayed results
-
+    sortResults() {
+      this.results.sort((a, b) => b.distance - a.distance);
+    },
+    quickSearch(event) {
+      if (this.results.length > 0) this.sortResults();
+      const input = event.target.value;
       if (input.length > 0) {
-        this.results = [];
-
         this.sections.forEach((section) => {
+          // subsections
           if (section?.type) {
             section.type.forEach((type) => {
-              const similarityScore = this.levenshteinDistance(
-                type.title.toLowerCase(),
-                input
-              );
-
-              const result = {
-                title: type.title,
-                href: type.href,
-                similarityScore: similarityScore,
-              };
-
-              this.results.push(result);
-              // if (similarityScore < similarityThreshold) {
-              //   this.results.push(result);
-              // }
+              this.updateResults(type, input);
             });
           } else {
-            const similarityScore = this.levenshteinDistance(
-              section.title.toLowerCase(),
-              input
-            );
-
-            const result = {
-              title: section.title,
-              href: section.href,
-              similarityScore: similarityScore,
-            };
-
-            this.results.push(result);
-            // if (similarityScore < similarityThreshold) {
-            //   this.results.push(result);
-            // }
+            this.updateResults(section, input);
           }
         });
-
-        // Sort the results based on similarity score
-        this.results.sort((a, b) => a.similarityScore - b.similarityScore);
-
-        // Limit the number of displayed results
-        this.results = this.results.slice(0, maxResults);
       } else {
         this.results = [];
       }
@@ -158,4 +115,9 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.input-content {
+  @apply absolute top-1/2;
+  translate: 0 -50%;
+}
+</style>
---
+++ components/search-menu.vue
@@ -0,0 +1,82 @@
+<template>
+  <ul class="menu bg-base-100 rounded-box app-border shadow-inner">
+    <template v-for="(result, index) in newResults">
+      <li :key="index" :id="`search-item-${index}`">
+        <a role="button" :href="result.href">{{ result.title }}</a>
+      </li>
+    </template>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    results: {
+      type: Array,
+      required: true,
+    },
+    maxResults: {
+      type: Number,
+      default: 3,
+    },
+  },
+  data() {
+    return {
+      selected: 0,
+    };
+  },
+  watch: {
+    results: {
+      handler() {
+        this.$nextTick(() => {
+          const firstSearchItem =
+            document.getElementById("search-item-0").firstChild;
+          firstSearchItem.classList.add("focus");
+          window.addEventListener(
+            "keydown",
+            (event) => {
+              if (event.code === "Enter") firstSearchItem.click();
+            },
+            { once: true }
+          );
+        });
+      },
+      immediate: true,
+    },
+  },
+  mounted() {
+    this.arrowNavigation();
+  },
+  computed: {
+    newResults() {
+      return this.results.slice(0, this.maxResults);
+    },
+  },
+  methods: {
+    arrowNavigation() {
+      window.addEventListener("keydown", (event) => {
+        if (document.getElementById("search-item-0")) {
+          const firstSearchItem =
+            document.getElementById("search-item-0").firstChild;
+          if (firstSearchItem.classList.contains("focus"))
+            firstSearchItem.classList.remove("focus");
+        }
+        if (event.code === "ArrowDown") {
+          event.preventDefault();
+          this.selected = (this.selected + 1) % this.newResults.length;
+          document
+            .getElementById(`search-item-${this.selected}`)
+            .firstChild.focus();
+        } else if (event.code === "ArrowUp") {
+          event.preventDefault();
+          this.selected =
+            (this.selected - 1 + this.maxResults) % this.newResults.length;
+          document
+            .getElementById(`search-item-${this.selected}`)
+            .firstChild.focus();
+        }
+      });
+    },
+  },
+};
+</script>
