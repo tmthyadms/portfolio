fa8d5ed772de67aa5778017e5106ab0f86476bd8
tmthyadms <tmthyadms@users.noreply.github.com>
2023-11-21 10:33:53 +0000

    deploy: f355d5803e402a696b671b73f8fd1e054f2c07a1

--- _nuxt/static/1700562361/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1700562361",layout:"default",error:a,state:{theme:{isDarkMode:false}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
