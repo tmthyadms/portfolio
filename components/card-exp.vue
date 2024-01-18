c80bc3cc572565ad9819eedfc60fcafd7e006723
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2024-01-19 05:01:24 +0800

    feat: add html support for desc

--- components/card-exp.vue
+++ components/card-exp.vue
@@ -1,54 +1,56 @@
 <template>
-  <AppCard class="group min-h-[24rem] app-border shadow-inner overflow-hidden">
+  <AppCard class="app-border group min-h-[24rem] overflow-hidden shadow-inner">
     <div
-      class="hidden lg:inline-block tooltip tooltip-secondary tooltip-left self-end"
+      class="tooltip tooltip-left tooltip-secondary hidden self-end lg:inline-block"
       data-tip="Current work"
     >
       <div
-        class="p-1.5 rounded-box outline outline-1 outline-base-content/[0.2]"
+        class="rounded-box p-1.5 outline outline-1 outline-base-content/[0.2]"
       >
         <IconBriefcase />
       </div>
     </div>
     <h2
-      class="m-auto font-serif font-black text-3xl lg:text-4xl text-base-content/[0.25]"
+      class="m-auto font-serif text-3xl font-black text-base-content/[0.25] lg:text-4xl"
       v-html="exp.company"
     ></h2>
     <div
-      class="space-y-2 lg:translate-y-2/3 lg:group-hover:translate-y-0 transition duration-500 ease-out"
+      class="space-y-2 transition duration-500 ease-out lg:translate-y-2/3 lg:group-hover:translate-y-0"
     >
       <div
-        class="text-start lg:-translate-y-full lg:group-hover:translate-y-0 transition-transform duration-500 ease-out"
+        class="text-start transition-transform duration-500 ease-out lg:-translate-y-full lg:group-hover:translate-y-0"
       >
-        <time class="text-sm font-mono"
+        <time class="font-mono text-sm"
           >{{ startDate }}&mdash;{{ endDate }}</time
         >
       </div>
       <h3
-        class="card-title text-lg lg:text-xl lg:-translate-y-full lg:group-hover:translate-y-0 transition-transform duration-500 ease-out"
+        class="card-title text-lg transition-transform duration-500 ease-out lg:-translate-y-full lg:text-xl lg:group-hover:translate-y-0"
       >
         {{ exp.role }}
         <IconArrowRight
-          class="hidden lg:block lg:group-hover:animate-none lg:group-hover:translate-x-1 lg:group-hover:opacity-0 transition duration-300"
+          class="hidden transition duration-300 lg:block lg:group-hover:translate-x-1 lg:group-hover:animate-none lg:group-hover:opacity-0"
         />
       </h3>
       <div
-        class="space-y-2 lg:opacity-0 lg:group-hover:opacity-100 transition-all duration-300 ease-out lg:group-hover:ease-in"
+        class="space-y-2 transition-all duration-300 ease-out lg:opacity-0 lg:group-hover:opacity-100 lg:group-hover:ease-in"
       >
         <div class="space-y-2" :class="{ 'opacity-60': exp?.desc }">
           <p
-            class="flex items-center lg:hidden gap-1 text-xs text-start opacity-60"
+            class="flex items-center gap-1 text-start text-xs opacity-60 lg:hidden"
           >
             <IconAsterisk :width="8" class="fill-secondary" />
             <span>Current work</span>
           </p>
-          <p v-if="exp?.desc" class="text-sm text-start opacity-60">
-            {{ exp.desc }}
-          </p>
+          <p
+            v-if="exp?.desc"
+            class="text-start text-sm opacity-60"
+            v-html="exp.desc"
+          ></p>
           <template v-else>
             <div class="flex flex-col gap-2">
-              <div class="skeleton w-full h-4"></div>
-              <div class="skeleton w-3/4 h-4"></div>
+              <div class="skeleton h-4 w-full"></div>
+              <div class="skeleton h-4 w-3/4"></div>
             </div>
             <!-- <span class="loading loading-dots loading-xs align-bottom"></span> -->
           </template>
@@ -57,7 +59,7 @@
           <div
             v-for="(skill, index) in exp.skills"
             :key="index"
-            class="badge badge-sm badge-outline badge-info"
+            class="badge badge-info badge-outline badge-sm"
           >
             {{ skill }}
           </div>
